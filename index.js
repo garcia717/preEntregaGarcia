d80b3b4e6e0aa5cb2f665b4586b3cba1d2beb47d
julio Garcia <garcia717@gmail.com>
2022-10-30 21:42:26 -0300

    Adelanto segunda entrega

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ let userName = prompt('¡Hola! Por favor ingresa tu nombre de usuario');
 let password = parseInt(prompt(`¡Bienvenido ${userName}! Por favor ingresa tu contraseña, recuerda solo tienes 3 intentos`));
 let trys = 0
 let correct = false
+let seguirComprando = true
+let totalCompra = 0
 if (password === 1234) {
     alert (`¡Maneja tu tienda ${userName}!`)
+    purchase()
 } else  if(password !== 1234){
     while (password !== 1234 && trys < 2){ 
-        let attemp = parseInt(prompt(`Contraseña incorrecta ${userName}, intenta de nuevo`))  
+        purchase()
         if (attemp === 1234){
             correct = true
             alert (`¡Maneja tu tienda ${userName}!`)
@@ -22,6 +25,44 @@ if(trys === 2){
 }
 }
 logIn()
+
+function purchase(){
+    let continueBuy = true
+    let total = 0
+    const productsArray =[]
+    const cart =[]
+    class NewProduct{
+        constructor(id,name,price,stock){
+          this.id = id
+          this.name = name;
+          this.price = price;
+          this.stock = stock
+        }
+      }
+    const product1 = new NewProduct('P1', 'Producto1', 100.00, 10)
+    productsArray.push(product1)
+    const product2 = new NewProduct('P2', 'Producto2', 300.00, 10)
+    productsArray.push(product2)
+    const product3 = new NewProduct('P3', 'Producto3', 50.00, 10)
+    productsArray.push(product3)
+    const product4 = new NewProduct('P4', 'Producto4', 500.00, 10)
+    productsArray.push(product4)
+    let productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toLocaleUpperCase()
+      if(productSelector === product1.id){
+        let subTotal = prompt(`El total de tu compra es ${total+product1.price}, ¿quieres seguir?`, 'Si o No')
+      }else{
+        alert('mamalo')
+      }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
