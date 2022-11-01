a141f29a937f9e2406233124c73617056e0ba2b1
julio Garcia <garcia717@gmail.com>
2022-11-01 13:53:22 -0300

    Revision CoderAsk

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ let trys = 0
 let correct = false
 if (password === 1234) {
     alert (`¡Maneja tu tienda ${userName}!`)
+    purchase()
 } else  if(password !== 1234){
     while (password !== 1234 && trys < 2){ 
         let attemp =parseInt(prompt(`Contraseña incorrecta ${userName}, intenta de nuevo`))
         if (attemp === 1234){
             correct = true
             alert (`¡Maneja tu tienda ${userName}!`)
+            purchase()
             break
         } else {
             trys++
@@ -24,6 +26,7 @@ if(trys === 2){
 function purchase(){
     let continueBuy = true
     let total = 0
+    let desicion 
     const productsArray =[]
     const cart =[]
     class NewProduct{
@@ -42,15 +45,22 @@ function purchase(){
     productsArray.push(product3)
     const product4 = new NewProduct('P4', 'Producto4', 500.00, 10)
     productsArray.push(product4)
-    let productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toLocaleUpperCase()
-      if(productSelector === product1.id){
-        let subTotal = prompt(`El total de tu compra es ${total+product1.price}, ¿quieres seguir?`, 'Si o No')
-      }else{
-        alert('mamalo')
+    let productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()
+    if(productSelector === productsArray.id){
+      let desicion = prompt(`El total de tu compra es ${total+totalPurchase}, ¿quieres seguir?`, 'Si o No').toUpperCase()
+    }else if(continueBuy === true){
+      while(continueBuy === true){
+        const product = productsArray.find(prod=>prod.id===productSelector)
+        cart.push(product)
+        totalPurchase = product.price
+        let desicion = prompt(`El total de tu compra es ${total+totalPurchase}, ¿quieres seguir?`, 'Si o No').toUpperCase()
+        productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()
+
       }
     }
+  }
     logIn()
-    purchase()
+
 
 
 
