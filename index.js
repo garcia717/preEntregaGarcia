7997dbd83f200c27cab154763ea59186152fd6b6
julio Garcia <garcia717@gmail.com>
2022-11-07 09:30:52 -0300

    segunda Preentrega Coder

--- index.js
+++ index.js
@@ -24,43 +24,42 @@ if(trys === 2){
 }
 }
 function purchase(){
-    let continueBuy = true
-    let total = 0
-    let desicion 
-    const productsArray =[]
-    const cart =[]
-    class NewProduct{
-        constructor(id,name,price,stock){
-          this.id = id
-          this.name = name;
-          this.price = price;
-          this.stock = stock
-        }
+  let productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()    
+  let continueBuy = true
+  let total = 0
+  let desicion 
+  const productsArray =[]
+  const cart =[]
+  class NewProduct{
+      constructor(id,name,price,stock){
+        this.id = id
+        this.name = name;
+        this.price = price;
+        this.stock = stock
       }
-    const product1 = new NewProduct('P1', 'Producto1', 100.00, 10)
-    productsArray.push(product1)
-    const product2 = new NewProduct('P2', 'Producto2', 300.00, 10)
-    productsArray.push(product2)
-    const product3 = new NewProduct('P3', 'Producto3', 50.00, 10)
-    productsArray.push(product3)
-    const product4 = new NewProduct('P4', 'Producto4', 500.00, 10)
-    productsArray.push(product4)
-    let productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()
-    if(productSelector === productsArray.id){
-      let desicion = prompt(`El total de tu compra es ${total+totalPurchase}, ¿quieres seguir?`, 'Si o No').toUpperCase()
-    }else if(continueBuy === true){
-      while(continueBuy === true){
-        const product = productsArray.find(prod=>prod.id===productSelector)
-        cart.push(product)
-        totalPurchase = product.price
-        let desicion = prompt(`El total de tu compra es ${total+totalPurchase}, ¿quieres seguir?`, 'Si o No').toUpperCase()
-        productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()
-
-      }
-    }else{
-      grandTotal = cart.map(product=>product.price).reduce((a,b)=>a+b)
-      alert(`total ${grandTotal}`)
     }
+  const product1 = new NewProduct('P1', 'Producto1', 100.00, 10)
+  productsArray.push(product1)
+  const product2 = new NewProduct('P2', 'Producto2', 300.00, 10)
+  productsArray.push(product2)
+  const product3 = new NewProduct('P3', 'Producto3', 50.00, 10)
+  productsArray.push(product3)
+  const product4 = new NewProduct('P4', 'Producto4', 500.00, 10)
+  productsArray.push(product4)
+  while(continueBuy === true){
+      const product = productsArray.find(prod=>prod.id===productSelector)
+      cart.push(product)
+      totalPurchase = product.price
+      desicion = parseInt(prompt(`El total de tu compra es US$ ${totalPurchase}, ¿quieres seguir?`, '1=Si ó 2=No'))
+      if(desicion === 1){
+          productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()
+          total = total + cart.map(prod=>prod.price).reduce((a,b)=>a+b)
+          console.log(cart);
+      }else{
+          continueBuy = false
+          alert(`Tu total a pagar es US$ ${total+totalPurchase}`)
+      }
+     }
   }
     logIn()
 
