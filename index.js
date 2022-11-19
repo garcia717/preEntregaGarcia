63a7fb362a0da44ed22ab36d89ecb910b46f373d
julio Garcia <garcia717@gmail.com>
2022-11-19 15:20:20 -0300

    Tercera entrega storage, DOM, Json

--- index.js
+++ index.js
@@ -1,67 +1,26 @@
-function logIn() {
-let userName = prompt('¡Hola! Por favor ingresa tu nombre de usuario');
-let password = parseInt(prompt(`¡Bienvenido ${userName}! Por favor ingresa tu contraseña, recuerda solo tienes 3 intentos`));
-let trys = 0
-let correct = false
-if (password === 1234) {
-    alert (`¡Maneja tu tienda ${userName}!`)
-    purchase()
-} else  if(password !== 1234){
-    while (password !== 1234 && trys < 2){ 
-        let attemp =parseInt(prompt(`Contraseña incorrecta ${userName}, intenta de nuevo`))
-        if (attemp === 1234){
-            correct = true
-            alert (`¡Maneja tu tienda ${userName}!`)
-            purchase()
-            break
-        } else {
-            trys++
-        }
+const formularioUsuario = document.getElementById('formulario')
+const titulo = document.getElementById('title')
+const nombreUsuario = document.getElementById('nombre')
+const apellidoUsuario = document.getElementById('apellido')
+const infoUsuario = {}
+
+formularioUsuario.onsubmit = (e) =>{
+    e.preventDefault()
+    infoUsuario.nombre = nombreUsuario.value
+    infoUsuario.apellido = apellidoUsuario.value
+    localStorage.setItem('infoUsuario',JSON.stringify(infoUsuario))
 }
-if(trys === 2){
-    alert(`Lamentamos que tengas problemas ${userName}, vuelve a intentarlo en 24 horas`)
+const infoUsuarioStorage = JSON.parse(localStorage.getItem('infoUsuario'))
+console.log(infoUsuarioStorage)
+if(infoUsuarioStorage.nombre !== "" || infoUsuarioStorage.apellido !== ""){
+    titulo.innerText = `Hola ${infoUsuarioStorage.nombre} ${infoUsuarioStorage.apellido}, continua comprando`
+    cerrar()
 }
+function cerrar() {
+  div = document.getElementById('obj1');
+  div.style.display = 'none';
 }
-}
-function purchase(){
-  let productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()    
-  let continueBuy = true
-  let total = 0
-  let desicion 
-  const productsArray =[]
-  const cart =[]
-  class NewProduct{
-      constructor(id,name,price,stock){
-        this.id = id
-        this.name = name;
-        this.price = price;
-        this.stock = stock
-      }
-    }
-  const product1 = new NewProduct('P1', 'Producto1', 100.00, 10)
-  productsArray.push(product1)
-  const product2 = new NewProduct('P2', 'Producto2', 300.00, 10)
-  productsArray.push(product2)
-  const product3 = new NewProduct('P3', 'Producto3', 50.00, 10)
-  productsArray.push(product3)
-  const product4 = new NewProduct('P4', 'Producto4', 500.00, 10)
-  productsArray.push(product4)
-  while(continueBuy === true){
-      const product = productsArray.find(prod=>prod.id===productSelector)
-      cart.push(product)
-      totalPurchase = product.price
-      desicion = parseInt(prompt(`El total de tu compra es US$ ${totalPurchase}, ¿quieres seguir?`, '1=Si ó 2=No'))
-      if(desicion === 1){
-          productSelector = prompt('Por favor selecciona tu producto, P1; P2; P3 o P4').toUpperCase()
-          total = total + cart.map(prod=>prod.price).reduce((a,b)=>a+b)
-          console.log(cart);
-      }else{
-          continueBuy = false
-          alert(`Tu total a pagar es US$ ${total+totalPurchase}`)
-      }
-     }
-  }
-    logIn()
+
 
 
 
